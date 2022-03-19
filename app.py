f3672d6d79c5808dc591f501adaaddaf1c11a9cf
Daniel Viscarra <dviscarr@espol.edu.ec>
2022-03-18 21:30:47 -0500

    Correccion libreria

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
-import plotly.express as px
+#import plotly.express as px
 
 st.title('Exportaciones no petroleras de Ecuador a Estados Unidos')
 
